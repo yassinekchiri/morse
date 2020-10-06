a9481e3c5dd8139c2c437bef4263c89a4ca4b563
KCHIRI Yassine <kchiri.yassine@gmail.com>
2020-10-06 16:40:31 +0200

    100% passing tests for one-word Morse decoder

--- morse.py
+++ morse.py
@@ -1,5 +1,38 @@
 # morse.py
 # pylint: disable=missing-docstring
+DICTIONNARY = {
+    '.-':   'A',
+    '-...': 'B',
+    '-.-.': 'C',
+    '-..':  'D',
+    '.':    'E',
+    '..-.': 'F',
+    '--.':  'G',
+    '....': 'H',
+    '..':   'I',
+    '.---': 'J',
+    '-.-':  'K',
+    '.-..': 'L',
+    '--':   'M',
+    '-.':   'N',
+    '---':  'O',
+    '.--.': 'P',
+    '--.-': 'Q',
+    '.-.':  'R',
+    '...':  'S',
+    '-':    'T',
+    '..-':  'U',
+    '...-': 'V',
+    '.--':  'W',
+    '-..-': 'X',
+    '-.--': 'Y',
+    '--..': 'Z'
+}
 
 def decode(message):
-    pass # TODO: implement the behavior!
+    if message == "":
+    	return ""
+    else:
+        message_alphabet = message.split(" ")
+        translated_message = [DICTIONNARY[alphabet] for alphabet in message_alphabet]
+        return ''.join(translated_message)
